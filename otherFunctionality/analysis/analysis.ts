4b2825a75a837eb6b64b163005e4b50065304a94
BossNumber1 <dimaang23@gmail.com>
2022-05-31 17:03:44 +0300

    Installed the typescript + renamed analysis.js on typescript format

---
+++ otherFunctionality/analysis/analysis.ts
@@ -0,0 +1,19 @@
+const matchingColors = require("../database/matchingColors");
+
+function analysis(firstColor, secondColor) {
+    // проверка на совместимость
+    for (let i = 0; i < matchingColors.length; i++) {
+        if (
+            (matchingColors[i].basic === firstColor &&
+                matchingColors[i].additional === secondColor) ||
+            (matchingColors[i].basic === secondColor &&
+                matchingColors[i].additional === firstColor)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+module.exports = { analysis };
