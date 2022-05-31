6ed22fd1b75de8720580c5e0635cc0b9ec09c84b
BossNumber1 <dimaang23@gmail.com>
2022-05-31 17:12:39 +0300

    added try-catch

--- otherFunctionality/commands/commands.js
+++ otherFunctionality/commands/commands.js
@@ -1,25 +1,29 @@
 module.exports = (bot, receivingSticker, startSearch) => {
     bot.on("message", async (msg) => {
-        const command = msg.text;
-        const chatId = msg.chat.id;
+        try {
+            const command = msg.text;
+            const chatId = msg.chat.id;
 
-        if (command === "/start") {
-            await receivingSticker(
+            if (command === "/start") {
+                await receivingSticker(
+                    chatId,
+                    "https://cdn.tlgrm.ru/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/256/4.webp"
+                );
+
+                return bot.sendMessage(chatId, `Поищем сочетание цветов?`);
+            }
+
+            if (command === "/search") {
+                startSearch(chatId);
+                return true;
+            }
+
+            return bot.sendMessage(
                 chatId,
-                "https://cdn.tlgrm.ru/stickers/c62/4a8/c624a88d-1fe3-403a-b41a-3cdb9bf05b8a/256/4.webp"
+                "Я тебя не понимаю. Со мной можно общаться лишь командами :)"
             );
-
-            return bot.sendMessage(chatId, `Поищем сочетание цветов?`);
+        } catch (e) {
+            console.error(e);
         }
-
-        if (command === "/search") {
-            startSearch(chatId);
-            return true;
-        }
-
-        return bot.sendMessage(
-            chatId,
-            "Я тебя не понимаю. Со мной можно общаться лишь командами :)"
-        );
     });
 };
